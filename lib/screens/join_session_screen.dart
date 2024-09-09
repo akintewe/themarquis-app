ccc51b25678aa65565f0ac48c02e6f04eb1bc1f4
co01y4p <yaphanyee@gmail.com>
2024-09-10 02:47:20 +0800

    before enhance open session scr

--- lib/screens/join_session_screen.dart
+++ lib/screens/join_session_screen.dart
@@ -17,12 +17,20 @@ class JoinSessionScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              'JOIN A SESSION',
+              'OPEN SESSION',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 60,
                 fontWeight: FontWeight.bold,
               ),
+            ),
+            roomCard(
+              roomName: "ROOM P8U7",
+              noOfPlayers: 3,
+            ),
+            roomCard(
+              roomName: "ROOM QW9K",
+              noOfPlayers: 1,
             ),
             TextButton(
               onPressed: () {
@@ -37,6 +45,51 @@ class JoinSessionScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget roomCard({
+    required String roomName,
+    required int noOfPlayers,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                roomName,
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w600),
+              ),
+              Text(
+                "${noOfPlayers}/4 Players",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              //four avatar
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+              ),
+              //join button
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "JOIN",
+                  style: TextStyle(
+                    fontSize: 38,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
