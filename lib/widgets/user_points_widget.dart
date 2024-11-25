c7db0761c733b7bb6828f065d26866ff84a875b9
hoossayn <abdulhakeemhusseni@gmail.com>
2024-11-25 18:14:25 +0100

    chore: (home screen) update home screen UI

---
+++ lib/widgets/user_points_widget.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../dialog/auth_dialog.dart';
+import '../providers/user.dart';
+
+
+class UserPointsWidget extends ConsumerStatefulWidget {
+  const UserPointsWidget({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<UserPointsWidget> createState() => _UserPointsWidgetState();
+}
+
+class _UserPointsWidgetState extends ConsumerState<UserPointsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final user = ref.watch(userProvider);
+    return GestureDetector(
+      onTap: user == null
+          ? () {
+        showDialog(
+            context: context,
+            builder: (c) => const AuthDialog());
+      }
+          : () {
+        //go to profile page
+      },
+      child: Row(
+        children: [
+          user == null
+              ? const Icon(
+            Icons.person,
+            size: 25,
+          )
+              : const CircleAvatar(
+            radius: 15,
+            backgroundImage: AssetImage(
+              'assets/images/avatar.png',
+            ), // Add your avatar image in assets folder
+            backgroundColor: Colors.transparent,
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                user == null
+                    ? "LOGIN"
+                    : user.email.split("@").first,
+                style: Theme.of(context)
+                    .textTheme
+                    .titleSmall!
+                    .copyWith(fontWeight: FontWeight.w600),
+              ),
+              user == null ? Container() :
+              Row(
+                children: [
+                  SizedBox(
+                    width: 12,
+                    child: Image.asset('assets/images/member.png'),
+                  ),
+                  const SizedBox(width: 5),
+                  Text(
+                    '${user.points.toString()} Pts.',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
