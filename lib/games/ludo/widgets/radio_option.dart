519e34f5fc887891b78bfd566766d260dd9ad4ee
Emrys <Emrys_02@outlook.com>
2024-11-29 21:35:09 +0100

    Radio Option Updated
    Signed-off-by: Emrys <Emrys_02@outlook.com>

--- lib/games/ludo/widgets/radio_option.dart
+++ lib/games/ludo/widgets/radio_option.dart
@@ -4,7 +4,6 @@ class RadioOption<T> extends StatelessWidget {
   final T? _value, _globalValue;
   final void Function(T) _onTap;
   final Color _selectedBackgroundColor, _unSelectedBackgroundColor, _borderColor;
-  final BoxShadow? _activeShadow;
   final double _width;
   final EdgeInsetsGeometry _padding;
   final Widget _child;
@@ -18,7 +17,6 @@ class RadioOption<T> extends StatelessWidget {
     required Color borderColor,
     required double width,
     required EdgeInsetsGeometry padding,
-    BoxShadow? activeShadow,
     required Widget child,
     super.key,
   })  : _value = value,
@@ -27,30 +25,53 @@ class RadioOption<T> extends StatelessWidget {
         _selectedBackgroundColor = selectedBackgroundColor,
         _unSelectedBackgroundColor = unSelectedBackgroundColor,
         _borderColor = borderColor,
-        _activeShadow = activeShadow,
         _width = width,
         _padding = padding,
         _child = child;
 
   bool get _isActive => _globalValue == _value && _value != null;
 
-  // Color get _backgroundColor => _isActive ? _selectedBackgroundColor : _unSelectedBackgroundColor;
+  Color get _backgroundColor => _isActive ? _selectedBackgroundColor : _unSelectedBackgroundColor;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _value == null ? null : () => _onTap(_value as T),
-      child: Container(
-        width: _width,
-        padding: _padding,
-        decoration: BoxDecoration(
-          // color: _backgroundColor,
-          border: _isActive ? null : Border.all(color: _borderColor),
-          boxShadow: [if (_isActive && _activeShadow != null) _activeShadow],
-          borderRadius: BorderRadius.circular(8),
+    return Container(
+      decoration: BoxDecoration(
+        color: _backgroundColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: GestureDetector(
+        onTap: _value == null ? null : () => _onTap(_value as T),
+        child: Container(
+          width: _width,
+          decoration: BoxDecoration(
+            border: Border.all(color: _isActive ? _backgroundColor : _borderColor),
+            gradient: !_isActive ? null : RadialGradient(colors: [Colors.transparent, Color(0xFF00ECFF).withOpacity(0.6)], radius: 1.7),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          clipBehavior: Clip.antiAlias,
+          alignment: Alignment.center,
+          child: Container(
+            padding: _padding,
+            width: _width,
+            decoration: BoxDecoration(
+              gradient: !_isActive
+                  ? null
+                  : LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color(0xFF00ECFF).withOpacity(0.6),
+                        Colors.transparent,
+                        Color(0xFF00ECFF).withOpacity(0.6),
+                      ],
+                      stops: [0.05, 0.4, 1],
+                    ),
+            ),
+            alignment: Alignment.center,
+            child: _child,
+          ),
         ),
-        alignment: Alignment.center,
-        child: _child,
       ),
     );
   }
