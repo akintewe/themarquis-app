1f954a549325aaf159ee4a9adab8d2d492617e9d
wms2537 <weimengsoh0827@gmail.com>
2025-01-23 10:40:26 +0800

    cleanup build

--- lib/env.dart
+++ lib/env.dart
@@ -1,11 +1,7 @@
 const environment = {
-  // 'apiUrl': 'http://localhost:8080',
-  'apiUrl': 'https://api.themarquis.xyz',
+  'apiUrl': 'https://staging.api.themarquis.xyz',
   'apiUrlDebug': 'https://staging.api.themarquis.xyz',
-  // 'apiUrlDebug': 'https://themarquis.home.wmtech.cc',
-  'wsUrl': 'wss://api.themarquis.xyz/ws',
+  'wsUrl': 'wss://staging.api.themarquis.xyz/ws',
   'wsUrlDebug': 'wss://staging.api.themarquis.xyz/ws',
-  // 'wsUrlDebug': 'wss://themarquis.home.wmtech.cc/ws',
-  // 'build': 'PROD',
-  'build': 'PROD',
+  'build': 'DEBUG',
 };
